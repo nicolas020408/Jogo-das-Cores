d18bc1bbe27aa4c22d0b8f4f5ab9f55d61f8732c
zaikodan <zaikodan@gmail.com>
2024-08-13 13:35:49 -0300

    Singleton implementado

--- Jogo das Cores/Assets/GameManager.cs
+++ Jogo das Cores/Assets/GameManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Singleton
+    public static GameManager instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        instance = this;
     }
+    #endregion
+
+
 }
