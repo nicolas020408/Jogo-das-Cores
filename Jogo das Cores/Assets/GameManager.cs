feaa9d55e6ea6bbabf6a86cee3aabe37a1a01414
zaikodan <zaikodan@gmail.com>
2024-08-13 13:55:30 -0300

    GerarSequencia implementado

--- Jogo das Cores/Assets/GameManager.cs
+++ Jogo das Cores/Assets/GameManager.cs
@@ -16,4 +16,23 @@ public class GameManager : MonoBehaviour
     int corDaVez, acertos, erros;
     int[] sequencia;
     [SerializeField] string[] nomes;
+
+    private void Start()
+    {
+        GerarSequencia();
+    }
+
+    void GerarSequencia()
+    {
+        corDaVez = 0;
+
+        sequencia = new int[Random.Range(3, nomes.Length)];
+        //UIManager.instance.LimparTexto();
+
+        for(int i = 0; i < sequencia.Length; i++) 
+        {
+            sequencia[i] = Random.Range(0, nomes.Length);
+            //UIManager.instance.AtualizarSequencia(nomes[sequencia[i]])
+        }
+    }
 }
